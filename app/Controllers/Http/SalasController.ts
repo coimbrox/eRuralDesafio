c1020471ee24ca52487a7b504d7d3e8987c74e3c
Gabriel Coimbra <gabao.coimbra@gmail.com>
2022-10-17 14:18:51 -0300

    sala controller

---
+++ app/Controllers/Http/SalasController.ts
@@ -0,0 +1,55 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Sala from 'App/Models/Sala'
+
+export default class SalasController {
+  public async index({}: HttpContextContract) {
+    try {
+      const salas = await Sala.all()
+      return salas
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  public async store({ request }: HttpContextContract) {
+    try {
+      const createData = request.all()
+
+      const sala = await Sala.create(createData)
+
+      return sala
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    try {
+      const sala = await Sala.findOrFail(params.id)
+      return sala
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    try {
+      const updateData = request.all()
+      const sala = await Sala.findOrFail(params.id)
+      sala.merge(updateData)
+      await sala.save()
+      return sala
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    try {
+      const sala = await Sala.findOrFail(params.id)
+      await sala.delete()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
